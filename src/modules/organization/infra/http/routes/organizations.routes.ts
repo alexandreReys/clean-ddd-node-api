b6315856649c77f491261a4b55f0d4a122b20b6d
alexandrereys <anr.alexandre@gmail.com>
2024-04-09 21:49:35 -0300

    refactor: remove organization create route used for testing

--- src/modules/organization/infra/http/routes/organizations.routes.ts
+++ src/modules/organization/infra/http/routes/organizations.routes.ts
@@ -6,9 +6,6 @@ import {
 
 const organizationRouter = Router();
 
-organizationRouter.post("/create", (request, response) => {
-  new OrganizationController().create(request, response);
-});
 organizationRouter.post("/", (request, response) => {
   new CreateOrganizationController().execute(request, response);
 });
